9af45bfa144228d0342e0aa34e9795ad56a7915a
Anh dai skeyler <bamboo2711@gmail.com>
2023-08-22 22:56:25 +0700

    Update

--- src/ConText/api.js
+++ src/ConText/api.js
@@ -2,66 +2,68 @@ import axios, { CancelToken } from "axios";
 import Toast from "react-native-toast-message";
 
 const callApi = async (
-  dispatch,
-  endpoint,
-  method,
-  data = null,
-  token = "",
-  params = null
+    dispatch,
+    endpoint,
+    method,
+    data = null,
+    token = "",
+    params = null
 ) => {
-  try {
-    const source = CancelToken.source();
+    try {
+        const source = CancelToken.source();
 
-    // Đặt thời gian chờ là 5 giây
-    const timeout = setTimeout(() => {
-      source.cancel("Request canceled due to timeout");
-    }, 5000);
+        // Đặt thời gian chờ là 5 giây
+        const timeout = setTimeout(() => {
+            source.cancel("Request canceled due to timeout");
+        }, 5000);
 
-    dispatch({ type: "SET_OVERLAY", payload: true });
-    const response = await axios.request({
-      baseURL: "http://27.74.240.29/apiPDM/",
-      timeout: 5000,
-      url: endpoint,
-      method: method,
-      data: data,
-      params: params,
-      headers: { Authorization: `Bearer ${token}` },
-      cancelToken: source.token,
-      // signal: AbortSignal.timeout(5000),
-    });
+        dispatch({ type: "SET_OVERLAY", payload: true });
+        const response = await axios.request({
+            baseURL: "http://27.74.240.29/apiPDM/",
+            timeout: 5000,
+            url: endpoint,
+            method: method,
+            data: data,
+            params: params,
+            headers: { Authorization: `Bearer ${token}` },
+            cancelToken: source.token,
+            // signal: AbortSignal.timeout(5000),
+        });
 
-    clearTimeout(timeout); // Xóa timeout khi request thành công
+        clearTimeout(timeout); // Xóa timeout khi request thành công
 
-    if (response.status == 200) {
-      dispatch({ type: "SET_OVERLAY", payload: false });
-      return response;
-    } else {
-      dispatch({ type: "SET_OVERLAY", payload: false });
-      Toast.show({
-        type: "error",
-        text1: "Thông báo",
-        text2: "Error",
-      });
-      return [];
+        if (response.status == 200) {
+            dispatch({ type: "SET_OVERLAY", payload: false });
+            return response;
+        } else {
+            dispatch({ type: "SET_OVERLAY", payload: false });
+            Toast.show({
+                type: "error",
+                text1: "Thông báo",
+                text2: "Error",
+            });
+            return [];
+        }
+    } catch (error) {
+        try {
+            const errorFields = Object.keys(error.response.data.errors);
+
+            // Truy cập giá trị lỗi của từng trường
+            errorFields.forEach((fieldName) => {
+                const errorText = error.response.data.errors[fieldName][0];
+                Toast.show({
+                    type: "error",
+                    text1: "Thông báo",
+                    text2: errorText,
+                });
+            });
+        } catch {}
+        // console.log(error.response.data.errors);
+
+        dispatch({ type: "SET_OVERLAY", payload: false });
+        // throw new Error(error.message);
+        return [];
     }
-  } catch (error) {
-    const errorFields = Object.keys(error.response.data.errors);
-
-    // Truy cập giá trị lỗi của từng trường
-    errorFields.forEach((fieldName) => {
-      const errorText = error.response.data.errors[fieldName][0];
-      Toast.show({
-        type: "error",
-        text1: "Thông báo",
-        text2: errorText,
-      });
-    });
-    // console.log(error.response.data.errors);
-
-    dispatch({ type: "SET_OVERLAY", payload: false });
-    // throw new Error(error.message);
-    return [];
-  }
 };
 
 // const callApi = async (endpoint, params, method = "GET", data = null) => {
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,186 +1,195 @@
-import { View, Text, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import React, { memo, useState } from "react";
 import { Dropdown, MultiSelect } from "react-native-element-dropdown";
 
 import colors from "../Common/colors";
 import { windowHeight } from "../Common/dimentions";
 
 const DropDown = ({
-  data,
-  placeholder,
-  labelField,
-  handleValue,
-  valueField,
-  multiselected = false,
-  ...props
+    data,
+    value,
+    placeholder,
+    labelField,
+    handleValue,
+    valueField,
+    multiselected = false,
+    ...props
 }) => {
-  const [focus, setFocus] = useState(false);
-  const [value, setValue] = useState("-1");
+    const [focus, setFocus] = useState(false);
 
-  const renderLabel = () => {
-    if (value != "" || focus) {
-      return (
-        <Text style={[styles.label, focus && { color: colors.primary }]}>
-          {placeholder}
-        </Text>
-      );
-    }
-    return null;
-  };
+    const renderLabel = () => {
+        if (value != "" || focus) {
+            return (
+                <Text
+                    style={[styles.label, focus && { color: colors.primary }]}
+                >
+                    {placeholder}
+                </Text>
+            );
+        }
+        return null;
+    };
 
-  const renderItem = (item, index) => {
+    const renderItem = (item, index) => {
+        return (
+            <View style={styles.item}>
+                <Text
+                    style={[
+                        styles.textItem,
+                        {
+                            color:
+                                item["color"] !== undefined
+                                    ? item["color"]
+                                    : colors.black,
+                        },
+                    ]}
+                >
+                    {item[labelField]}
+                </Text>
+            </View>
+        );
+    };
+
+    const [selected, setSelected] = useState([]);
+
     return (
-      <View style={styles.item}>
-        <Text
-          style={[
-            styles.textItem,
-            { color: item[valueField] % 2 === 1 ? "red" : "blue" },
-          ]}
-        >
-          {item[labelField]}
-        </Text>
-      </View>
+        <View style={styles.container}>
+            {renderLabel()}
+            {!multiselected ? (
+                <Dropdown
+                    style={[
+                        styles.dropdown,
+                        { borderColor: focus ? colors.primary : colors.border },
+                    ]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={data}
+                    search
+                    maxHeight={300}
+                    labelField={labelField}
+                    valueField={valueField}
+                    placeholder={!focus && placeholder}
+                    searchPlaceholder="Search..."
+                    value={value}
+                    onFocus={() => setFocus(true)}
+                    onBlur={() => setFocus(false)}
+                    onChange={(item, index) => handleValue(item)}
+                    renderItem={renderItem}
+                    {...props}
+                />
+            ) : (
+                <MultiSelect
+                    style={[
+                        styles.dropdown,
+                        { borderColor: focus ? colors.primary : colors.border },
+                    ]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={data}
+                    search
+                    maxHeight={300}
+                    labelField={labelField}
+                    valueField={valueField}
+                    placeholder={""}
+                    searchPlaceholder="Search..."
+                    value={selected}
+                    onFocus={() => setFocus(true)}
+                    onBlur={() => {
+                        setFocus(false);
+                    }}
+                    onChange={(item) => {
+                        setSelected(item);
+                    }}
+                    renderItem={renderItem}
+                    selectedStyle={{ borderRadius: 12 }}
+                    {...props}
+                />
+            )}
+        </View>
     );
-  };
-
-  const [selected, setSelected] = useState([]);
-
-  return (
-    <View style={styles.container}>
-      {renderLabel()}
-      {!multiselected ? (
-        <Dropdown
-          style={[
-            styles.dropdown,
-            { borderColor: focus ? colors.primary : colors.border },
-          ]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={data}
-          search
-          maxHeight={300}
-          labelField={labelField}
-          valueField={valueField}
-          placeholder={!focus && placeholder}
-          searchPlaceholder="Search..."
-          value={value}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          onChange={(item, index) => handleValue(item)}
-          renderItem={renderItem}
-          {...props}
-        />
-      ) : (
-        <MultiSelect
-          style={[
-            styles.dropdown,
-            { borderColor: focus ? colors.primary : colors.border },
-          ]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={data}
-          search
-          maxHeight={300}
-          labelField={labelField}
-          valueField={valueField}
-          placeholder={""}
-          searchPlaceholder="Search..."
-          value={selected}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          onChange={(item) => {
-            setSelected(item);
-          }}
-          renderItem={renderItem}
-          selectedStyle={{ borderRadius: 12 }}
-          {...props}
-        />
-      )}
-    </View>
-  );
 };
 
-export default DropDown;
+export default memo(DropDown);
 
 const styles = StyleSheet.create({
-  // dropdownStyle: {
-  //     paddingHorizontal: 10,
-  //     marginBottom: windowHeight / 60,
-  //     height: windowHeight / 18,
-  //     borderRadius: 5,
-  //     shadowColor: "gray",
-  //     shadowOffset: { width: 0, height: 2 },
-  //     shadowOpacity: 0.25,
-  //     shadowRadius: 3.85,
-  //     elevation: 5,
-  // },
-  // placeholderDropDown: {
-  //     fontSize: 16,
-  //     marginHorizontal: 15,
-  //     fontWeight: "400",
-  // },
-  // selectedDropDown: { fontSize: 16 },
-  // label: {
-  //     position: "absolute",
-  //     backgroundColor: "white",
-  //     left: 22,
-  //     top: 8,
-  //     zIndex: 999,
-  //     paddingHorizontal: 8,
-  //     fontSize: 14,
-  // },
-  container: {},
-  dropdown: {
-    paddingHorizontal: 10,
-    marginBottom: windowHeight / 60,
-    height: windowHeight / 18,
-    borderRadius: 5,
-    borderWidth: 1,
-    shadowColor: "gray",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.85,
-    elevation: 5,
-    backgroundColor: colors.backgroundColor,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: "absolute",
-    backgroundColor: colors.backgroundColor,
-    left: 10,
-    top: -8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
-  item: {
-    padding: 17,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  textItem: {
-    flex: 1,
-    fontSize: 16,
-  },
+    // dropdownStyle: {
+    //     paddingHorizontal: 10,
+    //     marginBottom: windowHeight / 60,
+    //     height: windowHeight / 18,
+    //     borderRadius: 5,
+    //     shadowColor: "gray",
+    //     shadowOffset: { width: 0, height: 2 },
+    //     shadowOpacity: 0.25,
+    //     shadowRadius: 3.85,
+    //     elevation: 5,
+    // },
+    // placeholderDropDown: {
+    //     fontSize: 16,
+    //     marginHorizontal: 15,
+    //     fontWeight: "400",
+    // },
+    // selectedDropDown: { fontSize: 16 },
+    // label: {
+    //     position: "absolute",
+    //     backgroundColor: "white",
+    //     left: 22,
+    //     top: 8,
+    //     zIndex: 999,
+    //     paddingHorizontal: 8,
+    //     fontSize: 14,
+    // },
+    container: {},
+    dropdown: {
+        paddingHorizontal: 10,
+        marginBottom: windowHeight / 60,
+        height: windowHeight / 18,
+        borderRadius: 5,
+        borderWidth: 1,
+        shadowColor: "gray",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.85,
+        elevation: 5,
+        backgroundColor: colors.backgroundColor,
+    },
+    icon: {
+        marginRight: 5,
+    },
+    label: {
+        position: "absolute",
+        backgroundColor: colors.backgroundColor,
+        left: 10,
+        top: -8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+    item: {
+        padding: 17,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    textItem: {
+        flex: 1,
+        fontSize: 16,
+    },
 });
